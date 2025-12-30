f7d333b05a9fbc907eff2138b723ca5a170f7f59
Your Name <your.email@example.com>
2025-12-30 21:27:30 +0530

    added certificates links

--- src/app/components/Certificates.tsx
+++ src/app/components/Certificates.tsx
@@ -6,37 +6,37 @@ export function Certificates() {
       title: "Data Analyst",
       issuer: "IBM",
       date: "Apr 2025",
-      url: "#"
+      url: "https://credentials.edx.org/credentials/ffed9c77c4f74a588edc91f7f2027743/"
     },
     {
       title: "Data Science Foundations – Level 2",
       issuer: "IBM",
       date: "Nov 2025",
-      url: "#"
+      url: "https://www.credly.com/badges/e63704e7-8970-45ce-92c0-2432211d4abf/linked_in_profile"
     },
     {
       title: "Data Science 101",
       issuer: "IBM SkillsBuild",
       date: "Nov 2025",
-      url: "#"
+      url: "https://courses.yl-ptech.skillsnetwork.site/certificates/597928c021094c82a6a8df8fa388d8ff"
     },
     {
       title: "Analyzing Data with Python",
       issuer: "edX",
       date: "Apr 2025",
-      url: "#"
+      url: "https://courses.edx.org/certificates/6a21d14dceda4b2ba3c8850cc3075930"
     },
     {
       title: "SQL for Data Science",
       issuer: "edX",
       date: "Mar 2025",
-      url: "#"
+      url: "https://courses.edx.org/certificates/dfc6862f7f524144be41505c732ad7e3"
     },
     {
       title: "Visualizing Data with Python",
       issuer: "edX",
       date: "Apr 2025",
-      url: "#"
+      url: "https://courses.edx.org/certificates/16df87430f7d4aeb8b9bd44e1c8c6424"
     },
     {
       title: "Analyzing & Visualizing Data with Power BI",
@@ -48,19 +48,19 @@ export function Certificates() {
       title: "Power BI Essential Training",
       issuer: "LinkedIn Learning",
       date: "Jun 2025",
-      url: "#"
+      url:"https://courses.edx.org/certificates/228724109ba741e896da35ad5338e954"
     },
     {
       title: "AWS Academy Graduate – Cloud Architecting",
       issuer: "Amazon Web Services (AWS)",
       date: "Oct 2025",
-      url: "#"
+      url: "https://www.credly.com/badges/99561353-fc1c-4670-9839-d4fc6f4733bc/linked_in_profile"
     },
     {
       title: "Foundations of Data Structures & Algorithm Analysis",
       issuer: "Packt",
       date: "Oct 2025",
-      url: "#"
+      url: "https://www.coursera.org/account/accomplishments/verify/BOOUG5OJNQLF"
     }
   ];
 
